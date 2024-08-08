27a2716eb277162931c5aefa4095cc1e500606b4
Akanksha Kumari <115232645+akankshaarushi@users.noreply.github.com>
2024-08-08 15:35:57 +0530

    Update SideBar.jsx

--- src/Components/sidebar/SideBar.jsx
+++ src/Components/sidebar/SideBar.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './Sidebar.css';
-import DashboardIcon from '@mui/icons-material/Dashboard';
+import HomeIcon from '@mui/icons-material/Home';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import MailIcon from '@mui/icons-material/Mail';
 import AssignmentIcon from '@mui/icons-material/Assignment';
-import ListIcon from '@mui/icons-material/List';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar-icon"><DashboardIcon /></div>
-      <div className="sidebar-icon"><AssignmentIcon /></div>
-      <div className="sidebar-icon"><ListIcon /></div>
+      <div className="sidebar-icons">
+        <div className="sidebar-icon"><HomeIcon /></div>
+        <div className="sidebar-icon"><BarChartIcon /></div>
+        <div className="sidebar-icon"><MailIcon /></div>
+        <div className="sidebar-icon"><AssignmentIcon /></div>
+        <div className="sidebar-icon"><ShoppingCartIcon /></div>
+      </div>
+      <div className="sidebar-icon sidebar-icon-settings"><SettingsIcon /></div>
     </div>
   );
 }
